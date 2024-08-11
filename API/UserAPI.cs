8c243bea2c17d5aca0cafa485b07ffa5ed4b7a68
Gregory Markus <gregmarkus1992@yahoo.com>
2024-08-11 10:59:59 -0500

    user api has been updated. has crud capabilities

--- API/UserAPI.cs
+++ API/UserAPI.cs
@@ -1,6 +1,69 @@
-﻿namespace Shredule.API
+﻿using shredule.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shredule.API
 {
     public class UserAPI
     {
+        public static void Map(WebApplication app)
+        {
+            app.MapGet("/users/{id}", (ShreduleDbContext db, int id) => // get user by id
+            {
+                var user = db.Users.FirstOrDefault(u => u.Id == id);
+
+                if (user == null)
+                {
+                    return Results.NotFound("");
+                }
+
+                return Results.Ok(user);
+            });
+
+            app.MapGet("/checkuser/{id}", (ShreduleDbContext db, int id) => // check for user
+            {
+                var user = db.Users.FirstOrDefault(u => u.Id == id);
+
+                if (user == null)
+                {
+                    return Results.NotFound("");
+                }
+
+                return Results.Ok(user);
+            });
+
+            app.MapPost("/users/register", (ShreduleDbContext db, User newUser) => //register user
+            {
+                try
+                {
+                    db.Users.Add(newUser);
+                    db.SaveChanges();
+                    return Results.Created($"/users/{newUser.Id}", newUser);
+                }
+                catch (DbUpdateException)
+                {
+                    return Results.BadRequest("");
+                }
+            });
+
+            app.MapPut("/users/{id}", (ShreduleDbContext db, int id, User user) => // update user
+            {
+                var userBeingUpdated = db.Users.FirstOrDefault(u => u.Id == id);
+
+                if (userBeingUpdated == null)
+                {
+                    return Results.NotFound("No user found");
+                }
+
+                userBeingUpdated.Id = user.Id;
+                userBeingUpdated.UserName = user.UserName;
+                userBeingUpdated.Name = user.Name;
+                userBeingUpdated.Password = user.Password;
+                db.SaveChanges();
+                return Results.Ok("User has been updated");
+            });
+
+
+
+        }
     }
 }
