7913f6060ddf8a465f0aa6709ed5520f95dfa976
Gregory Markus <gregmarkus1992@yahoo.com>
2024-08-13 17:41:26 -0500

    crud on bands has been made

--- API/BandAPI.cs
+++ API/BandAPI.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using shredule.Models;
+using Shredule.Models;
 
 namespace Shredule.API
 {
@@ -19,6 +21,71 @@ namespace Shredule.API
                 }
 
             });
+
+            app.MapGet("/bands/band/{id}", (ShreduleDbContext db, int id) =>
+            {
+                var thisBand = db.Bands.FirstOrDefault(b => b.Id == id);
+                if (thisBand != null)
+                {
+                    return Results.Ok(thisBand);
+                }
+                else
+                {
+                    return Results.NotFound("");
+                }
+            });
+
+            app.MapDelete("/bands/{id}", (ShreduleDbContext db, int id) =>
+            {
+                var bandToDelete = db.Bands.FirstOrDefault(bm => bm.Id == id);
+                if (bandToDelete != null)
+                {
+                    db.Bands.Remove(bandToDelete);
+                    db.SaveChanges();
+                    return Results.Ok("This band has been deleted");
+                }
+                else
+                {
+                    return Results.NotFound("");
+                }
+            });
+
+            app.MapPost("/bands", (ShreduleDbContext db, NewBandDTO bandToCreate) =>
+            {
+                try
+                {
+                    Band newBand = new()
+                    {
+                        Id = bandToCreate.Id,
+                        Name = bandToCreate.Name,
+                        Password = bandToCreate.Password,
+                    };
+                    db.Bands.Add(newBand);
+                    db.SaveChanges();
+                    return Results.Created($"/bands/{newBand.Id}", newBand);
+                }
+                catch (DbUpdateException)
+                {
+                    return Results.BadRequest("Unable to create band, check your data and try again");
+                }
+            });
+
+            app.MapPut("/bands/{id}", (ShreduleDbContext db, int id, NewBandDTO updatedBand) =>
+            {
+                var bandToUpdate = db.Bands.FirstOrDefault();
+                if (bandToUpdate != null)
+                {
+                    bandToUpdate.Name = updatedBand.Name;
+                    bandToUpdate.Password = updatedBand.Password;
+                    db.SaveChanges();
+                    return Results.Created($"/bands/{updatedBand.Id}", updatedBand);
+                }
+                else
+                {
+                    return Results.BadRequest();
+                }
+            });
         }
     }
 }
+
---
+++ Models/NewBandDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Shredule.Models
+{
+    public class NewBandDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+    }
+}
