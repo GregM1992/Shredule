3ca318fa7bfb6c57045e7cb345bd775f71ec5310
Gregory Markus <gregmarkus1992@yahoo.com>
2024-08-18 09:42:11 -0500

    added api for practices

--- API/PracticeAPI.cs
+++ API/PracticeAPI.cs
@@ -1,6 +1,59 @@
-﻿namespace Shredule.API
+﻿using Microsoft.EntityFrameworkCore;
+using Shredule.Models;
+
+namespace Shredule.API
 {
     public class PracticeAPI
     {
+        public static void Map(WebApplication app)
+        {
+            app.MapGet("/bandPractice/{bandId}", (ShreduleDbContext db, int bandId) =>
+            {
+                var bandPractices = db.Practices.Where(p => p.BandId == bandId).ToList();
+                if (bandPractices.Count > 0)
+                {
+                    return Results.Ok(bandPractices);
+                }
+                else
+                {
+                    return Results.NotFound("");
+                }
+
+            });
+
+            app.MapPost("/bandPractice/{bandId}", (ShreduleDbContext db, int bandId, DateTime newDate, Practice newPractice) =>
+            {
+                try
+                {
+                    newPractice.BandId = bandId;
+                    newPractice.DateTime = newDate;
+                    db.Practices.Add(newPractice);
+                    db.SaveChanges();
+                    return Results.Created($"/bandPractices/{newPractice.Id}", newPractice);
+                }
+                catch (DbUpdateException)
+                {
+                    return Results.BadRequest("Unable to create practice");
+                }
+
+            });
+
+            app.MapDelete("/bandPractice/{practiceId}", (ShreduleDbContext db, int practiceId) =>
+            {
+                var practiceToDelete = db.Practices.FirstOrDefault(p => p.Id == practiceId);
+                if (practiceToDelete != null)
+                {
+                    db.Practices.Remove(practiceToDelete);
+                    db.SaveChanges();
+                    return Results.Ok("Practice has been successfully removed");
+                }
+                else
+                {
+                    return Results.NotFound("This practice does not exist or has already been deleted");
+                }
+
+
+            });
+        }
     }
 }
---
+++ Migrations/20240818142059_PracticeLocation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shredule.Migrations
+{
+    /// <inheritdoc />
+    public partial class PracticeLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Practices",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Practices",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Location",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Practices");
+        }
+    }
+}
