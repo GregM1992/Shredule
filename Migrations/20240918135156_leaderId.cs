ef34781660db7b9f8fb06ec868b078bf238f90b3
Gregory Markus <gregmarkus1992@yahoo.com>
2024-09-18 08:52:39 -0500

    added leaderId to band model and migrated

---
+++ Migrations/20240918135156_leaderId.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shredule.Migrations
+{
+    /// <inheritdoc />
+    public partial class leaderId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LeaderId",
+                table: "Bands",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Bands",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LeaderId",
+                value: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LeaderId",
+                table: "Bands");
+        }
+    }
+}
